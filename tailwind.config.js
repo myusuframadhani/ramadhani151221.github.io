4aebd8230e4e6796b22aab189942f45787dffccc
ramadhani151221 <202410101050@mail.unej.ac.id>
2022-07-05 22:54:59 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "ui-sans-serif", "system-ui"],
+    },
+  },
+  plugins: [],
+};
